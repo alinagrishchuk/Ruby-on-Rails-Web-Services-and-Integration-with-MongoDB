cadd8af439200db65477090b04002bed8b4fcf92
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-05-15 19:14:27 +0300

    add 1:1 Linked relations

--- module3/Lesson02-Mongoid-Relationships/contests/app/models/racer.rb
+++ module3/Lesson02-Mongoid-Relationships/contests/app/models/racer.rb
@@ -6,11 +6,14 @@ class Racer
   field :dob, as: :date_of_birth, type: Date
 
   embeds_one :primary_address,  class_name: 'Address', as: :addressable
+  has_one :medical_record
 
   def races
-    Contest
-      .where('entrants.racer_id': BSON::ObjectId.from_string(self.id))
-      .map{ |contest| contest.entrants.where(racer_id: self.id).first }
+    if self.id
+      Contest
+        .where('entrants.racer_id': BSON::ObjectId.from_string(self.id))
+        .map{ |contest| contest.entrants.where(racer_id: self.id).first }
+    end
   end
 
 end
