0836edba5d90bebbc60ec13513b7c991094ad62d
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-05-16 13:09:45 +0300

    implement races/results cross-collection relationships

--- module3/triresults/app/models/entrant.rb
+++ module3/triresults/app/models/entrant.rb
@@ -11,9 +11,13 @@ class Entrant
 
   embeds_many :results, class_name: 'LegResult',order: [:'event.o'.asc],
               after_add: :update_total, after_remove: :update_total
+  embeds_one :race, class_name: 'RaceRef'
 
   def update_total(result)
     self.secs = results.inject(0){|sum,x| x.secs ? sum + x.secs : sum}
   end
 
+  def the_race
+    self.race.race if self.race
+  end
 end
--- module3/triresults/app/models/race.rb
+++ module3/triresults/app/models/race.rb
@@ -7,7 +7,9 @@ class Race
   field :loc, as: :location, type: Address
 
   embeds_many :events, as: :parent, order: [:order.asc]
+  has_many :entrants, foreign_key: 'race._id', dependent: :delete
 
+  default_scope -> {order_by([:secs.asc, :bib.asc])}
   scope :upcoming, -> {where(:date.gte =>  Date.today)}
   scope :past, -> {where(:date.lt =>  Date.today)}
 end
